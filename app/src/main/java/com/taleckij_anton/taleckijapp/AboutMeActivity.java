d1362633cd9520f42c4d726a8bf7036602e824df
ByMyTry <anton.taleckij@gmail.com>
2018-01-26 03:28:26 +0300

    init

---
+++ app/src/main/java/com/taleckij_anton/taleckijapp/AboutMeActivity.java
@@ -0,0 +1,16 @@
+package com.taleckij_anton.taleckijapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class AboutMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_about_me);
+    }
+}
