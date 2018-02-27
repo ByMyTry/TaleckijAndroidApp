783f05beb2676058603efb7c2e095118d6a28699
ByMyTry <anton.taleckij@gmail.com>
2018-02-28 00:37:24 +0300

    AppMetrica & Firebase simple push

--- app/src/main/java/com/taleckij_anton/taleckijapp/App.java
+++ app/src/main/java/com/taleckij_anton/taleckijapp/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.yandex.metrica.YandexMetrica;
 import com.yandex.metrica.YandexMetricaConfig;
+import com.yandex.metrica.push.YandexMetricaPush;
 
 /**
  * Created by Lenovo on 11.02.2018.
@@ -21,5 +22,7 @@ public class App extends Application {
         metricaConfigBuilder.handleFirstActivationAsUpdate(true);
         YandexMetrica.activate(getApplicationContext(), metricaConfigBuilder.build());
         YandexMetrica.enableActivityAutoTracking(this);
+
+        YandexMetricaPush.init(getApplicationContext());
     }
 }
